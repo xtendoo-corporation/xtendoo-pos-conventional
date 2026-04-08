c768c52129cf2b67770315e35afc6895bce85eba
manuelcalerosolis <manuelcalerosolis@gmail.com>
2026-04-08 07:53:28 +0200

    chore(pos_conventional_core): register test_pos_order_pricelist in tests __init__

--- pos_conventional_core/tests/__init__.py
+++ pos_conventional_core/tests/__init__.py
@@ -2,5 +2,6 @@ from . import common
 from . import test_pos_config
 from . import test_pos_order
 from . import test_pos_order_line_cost_margin
+from . import test_pos_order_pricelist
 from . import test_receipt_print
 from . import test_res_config_settings
