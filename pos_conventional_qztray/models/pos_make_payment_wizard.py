9378225f3c40b71f00bb6651ca3f2736519c13e9
Gokac9 <theabraham9@gmail.com>
2026-06-24 10:18:47 +0200

    fix impresion pequeña

--- pos_conventional_qztray/models/pos_make_payment_wizard.py
+++ pos_conventional_qztray/models/pos_make_payment_wizard.py
@@ -4,8 +4,20 @@ from odoo import models
 class PosMakePaymentWizard(models.TransientModel):
     _inherit = "pos.make.payment.wizard"
 
+    def _pos_conventional_qztray_enrich_wizard_action(self, action):
+        self.ensure_one()
+        if self.order_id:
+            return self.order_id._pos_conventional_qztray_enrich_print_action(action)
+        return action
+
     def _execute_validation(self, print_invoice=False):
         action = super()._execute_validation(print_invoice=print_invoice)
-        if self.order_id:
-            action = self.order_id._pos_conventional_qztray_enrich_print_action(action)
-        return action
+        return self._pos_conventional_qztray_enrich_wizard_action(action)
+
+    def action_validate(self):
+        action = super().action_validate()
+        return self._pos_conventional_qztray_enrich_wizard_action(action)
+
+    def action_validate_print(self):
+        action = super().action_validate_print()
+        return self._pos_conventional_qztray_enrich_wizard_action(action)
