7b866ecad7268f8aa36c2c7b29c865595bfd8301
Gokac9 <theabraham9@gmail.com>
2026-06-30 13:47:20 +0200

    report like odoo v2

--- pos_conventional_qztray/models/ir_actions_report.py
+++ pos_conventional_qztray/models/ir_actions_report.py
@@ -1,10 +1,12 @@
-from odoo import models
+from odoo import api, models
 
 
 class IrActionsReport(models.Model):
     _inherit = "ir.actions.report"
 
-    def print_action_for_report_name(self, report_name):
+    @api.model
+    def print_action_for_report_name(self, *args):
+        report_name = args[0] if args else self.env.context.get("report_name")
         if report_name == "pos_conventional_qztray.report_factura_simplificada_80mm_standard_qztray":
             report_name = "pos_conventional_receipt_custom.report_factura_simplificada_80mm"
         return super().print_action_for_report_name(report_name)
